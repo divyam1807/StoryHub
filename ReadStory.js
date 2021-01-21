f4ce6f8ed03ae365f98e5617ea72f8d86f958608
divyam1807 <59440388+divyam1807@users.noreply.github.com>
2021-01-21 16:09:02 +0530

    Add files via upload

--- ReadStory.js
+++ ReadStory.js
@@ -2,6 +2,20 @@ import * as React from 'react';
 import {StyleSheet,View,Text} from 'react-native';
 
 export default class ReadStory extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            allStories:[]
+        }
+    }retrieveStories=()=>{
+        var allStories = []
+        var Stories = db.collection('Stories').get().then((Snapshot)=>{
+Snapshot.forEach(doc => {
+    allStories.push(doc.data())
+});
+this.setState({allStories})
+        })
+    }
     render(){
         return(
             <Text>Welcom to ReadStory screen</Text>
